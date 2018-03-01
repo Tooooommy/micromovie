5f10e1306046c777a62c124da68a501200848612
Tommy <1520752231@qq.com>
2018-03-01 21:55:42 +0800

    v1

---
+++ app/home/__init__.py
@@ -0,0 +1,8 @@
+#！/usr/bin/python3
+# _*_ coding:utf-8 _*_
+
+from flask import Blueprint
+
+home = Blueprint("home", __name__)
+
+import app.home.views
---
+++ manage.py
@@ -0,0 +1,7 @@
+#！/usr/bin/python3
+# _*_ coding:utf-8 _*_
+
+from app import app
+
+if __name__ == "__main__":
+    app.run()
